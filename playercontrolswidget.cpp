d8d88131eccce4d3b930d41be31d20b62cc38503
Harrison Voss <hvoss@iastate.edu>
2025-10-06 02:03:18 -0500

    reverted volume calc, fixed bug

--- playercontrolswidget.cpp
+++ playercontrolswidget.cpp
@@ -66,7 +66,7 @@ void PlayerControlsWidget::on_Volume_valueChanged(int value)
 {
     if (!player) return;
 
-    player->audioOutput()->setVolume(value/100);
+    player->audioOutput()->setVolume(value * 0.01);
 }
 
 
